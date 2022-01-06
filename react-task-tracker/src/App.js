05fe51b3f6abc161e555b5b396b0346cc1fa4d1f
mario <sumario82@gmail.com>
2022-01-05 22:36:22 -0500

    made a component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,16 @@
+import Header from './components/Header'
 
 function App() {
+  const name = 'brad'
+  const x = false
+
   return (
-    <div className="App">
+    <div className='container'>
       <h1>hello from react</h1>
+      <h2>other thing {name}</h2>
+      <h3>{1+2}</h3>
+      <h4>{x ? 'yes' : 'no'}</h4>
+      <Header/>
     </div>
   );
 }
